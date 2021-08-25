11bc32c8306a2e2fb065635010a1ae59bf7053e0
jaredl85 <jaredloveridge@gmail.com>
2021-08-25 10:26:41 -0700

    added simple module

--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,14 @@
-exports.perimeter = (x, y) => (x + y);
-exports.area = (x, y) => x * y;
+module.exports = (x, y, callback) => {
+    if (x <= 0 || y <= 0) {
+        callback(new Error(`Rectangle dimensions must be greater than zero. Received: ${x}, ${y}`))
+    } else {
+        setTimeout(() => 
+           callback(null, {
+            perimeter: () => (x + y),
+            area: () => x * y
+           }),
+           2000
+        );
+    }
+};
+
